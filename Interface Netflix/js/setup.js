2fcc4c54c93aee5417cc638b8454f08897476d60
GuiSalles17 <96488343+GuiSalles17@users.noreply.github.com>
2022-04-29 16:15:27 -0300

    Add files via upload

---
+++ Interface Netflix/js/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:13,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        370:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:7
+        }
+    }
+})
